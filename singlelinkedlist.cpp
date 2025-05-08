dd37e475490465124789b8e17bb240db6cab822d
26nafis <spalanzaninafis@gmail.com>
2025-05-08 20:05:06 +0700

    untuk  memasukan nim dan mengecek elemen

--- singlelinkedlist.cpp
+++ singlelinkedlist.cpp
@@ -57,4 +57,21 @@ public:
         previous->next = nodebaru;
     }
 
+    bool ListEmpty()
+    {
+        return START == NULL;
+    }
+
+    bool Search(int nim, Node **previous, Node **current)
+    {
+        *previous = START;
+        *current = START;
+        while (*current != NULL && nim != (*current)->noMhs)
+        {
+            *previous = *current;
+            *current = (*current)->next;
+        }
+        return (*current != NULL);
+    }
+
 }
